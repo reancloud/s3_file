cf4a2306baf7f039ee76c5203333b40b6e244f61
Noel Georgi <noel.georgi@reancloud.com>
2017-09-20 09:09:07 +0530

    Adding the compile time run to the package
    Signed-off-by: Noel Georgi <noel.georgi@reancloud.com>

--- recipes/dependencies.rb
+++ recipes/dependencies.rb
@@ -2,8 +2,8 @@ case node['platform_family']
 when 'debian', 'rhel', 'centos', 'amazon'
   %w(gcc gcc-c++).each do |pkg|
     package pkg
-  end
-end.run_action(:install)
+  end.run_action(:install)
+end
 
 chef_gem 'mime-types' do
   version node['s3_file']['mime-types']['version']
