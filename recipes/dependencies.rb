ee97e6d9df0cbb1427c656a67060ffdfeb332433
hiteshri <hiteshri.ramteke@reancloud.com>
2017-09-20 14:49:50 +0530

    rest-client issue fixed for RHEL-7 and Ubuntu-16.04

--- recipes/dependencies.rb
+++ recipes/dependencies.rb
@@ -1,3 +1,23 @@
+case node['platform_family']
+when 'rhel', 'centos', 'amazon'
+  package 'gcc' do
+    action :install
+  end.run_action(:install)
+  package 'gcc-c++' do
+    action :install
+  end.run_action(:install)
+when 'debian'
+  apt_update 'update' do
+    action :nothing
+  end.run_action(:periodic)
+  package 'g++' do
+    action :nothing
+  end.run_action(:install)
+  package 'make' do
+    action :nothing
+  end.run_action(:install)
+end
+
 chef_gem 'mime-types' do
   version node['s3_file']['mime-types']['version']
   action :install
